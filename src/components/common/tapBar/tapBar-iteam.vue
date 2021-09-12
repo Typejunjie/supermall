8257df6ee56316fabf6d55cf0b37ce2ab021ea4e
Typejunjie <2282756118@qq.com>
2021-09-12 20:28:35 +0800

    dang

---
+++ src/components/common/tapBar/tapBar-iteam.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="iteam" @click="iteamclick()">
+      <slot name='iteamimg'></slot>
+      <slot name='iteamtext'></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tapBariteam',
+    data () {
+        return {
+        }
+    },
+    props: ['path'],
+    methods: {
+        iteamclick() {
+            this.$router.replace(this.path)
+        }
+    }
+}
+</script>
+
+<style>
+@import url('../../assets/reset.css');
+
+.iteam{
+    font-size: 14px;
+    text-align: center;
+    margin-top: 5px;
+}
+.iteam :first-child{
+    font-size: 22px;
+    margin-bottom: 5px;
+}
+</style>
---
+++ src/components/content/tapBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="tapBar">
+    <tap-bariteam :path="'/home'">
+      <div class="fas fa-home" slot="iteamimg"></div>
+      <div slot="iteamtext">首页</div>
+    </tap-bariteam>
+    <tap-bariteam :path="'/cart'">
+      <div class="fas fa-folder" slot="iteamimg"></div>
+      <div slot="iteamtext">档案</div>
+    </tap-bariteam>
+    <tap-bariteam :path="'/shop'">
+      <div class="fas fa-shopping-cart" slot="iteamimg"></div>
+      <div slot="iteamtext">购物车</div>
+    </tap-bariteam>
+    <tap-bariteam :path="'/self'">
+      <div class="fas fa-user-tie" slot="iteamimg"></div>
+      <div slot="iteamtext">我的</div>
+    </tap-bariteam>
+  </div>
+</template>
+
+<script>
+import tapBariteam from '../common/tapBar-iteam.vue'
+
+export default {
+  name: "tapBar",
+  components: {
+    tapBariteam
+  },
+};
+</script>
+
+<style>
+.tapBar {
+  height: 49px;
+  width: 100%;
+  background-color: rgb(240, 239, 239);
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  box-shadow: 0 -2px 10px #d4d3d3;
+  display: flex;
+  justify-content: space-around;
+}
+</style>
