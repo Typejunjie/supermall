4a77fe2aedba3a765bfe2b6f1cfe413e38482ba3
Typejunjie <2282756118@qq.com>
2021-09-16 10:21:39 +0800

    完成nav使用

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -1,11 +1,20 @@
 <template>
     <div>
-        shop
+        <navtag :bgcolor='"background: #bfa"'>
+           <div slot="left"></div>
+           <div slot="content">购物车</div>
+           <div slot="right"></div>
+        </navtag>
     </div>
 </template>
 <script>
+import navtag from 'components/common/navtag.vue'
+
 export default {
-    name: 'shop'
+    name: 'shop',
+    components: {
+        navtag
+    }
 }
 </script>
 <style>
