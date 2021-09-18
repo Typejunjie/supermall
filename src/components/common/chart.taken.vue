0541b8f48cd013c7390fd73e8690185c9f568e84
Typejunjie <2282756118@qq.com>
2021-09-18 17:57:43 +0800

    初步轮播图完成

--- src/components/common/chart.taken.vue
+++ src/components/common/chart.taken.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="chartimg" @loade='chart()' @mousedown="down()" @mouseup="up()">
-        <slot></slot>
+    <div class="chartimg">
+        <div @mousedown="down" @mouseup="up" ref="img" :class="run"><slot></slot></div>
     </div>
 </template>
 
@@ -9,30 +9,63 @@ export default {
    name: 'chart',
    data () {
        return {
-           
+           runing: []//接收字符串指定动作
+       }
+   },
+   computed: {
+       //将runing的数组数据转为class可识别字符串
+       run() {
+           return this.runing.join(' ')
+       },
+       //图片的基础属性
+       massage() {
+           let img = this.$refs.img.children[0]
+           return {width: img.offsetWidth}
        }
    },
    methods: {
-       chart() {
-
+       //主要控制函数
+       taken(common) {
+           switch(common) {
+               case 'take': {
+                   this.time = setInterval(()=>{
+                       this.$refs.img.style.marginLeft = -this.massage.width + 'px'
+                       this.$refs.img.style.transition = '1s'
+                   },2000);break;
+               }
+               case 'stop': {
+                   clearInterval(this.time)
+               }
+           }
        },
-       down() {
-
+       //鼠标按下
+       down(event) {
+           this.taken('stop')
        },
-       up() {
-           
+       //鼠标弹起
+       up(event) {
+
        }
+   },
+   created () {
+   },
+   //元素挂载完成给予轮播
+   mounted () {
+       this.taken('take')
    }
 }
 </script>
 
 <style>
    .chartimg{
-       width: 100%;
-       height: 200px;
+       width: 375px;
        overflow: hidden;
    }
+   .chartimg div{
+       display: flex;
+   }
    .chartimg img{
-       width: 100%;
+       width: 375px;
+       vertical-align: bottom;
    }
 </style>
