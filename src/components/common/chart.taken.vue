9797f8284e0f4c9bac37cc4313448e5f95d51a9e
Typejunjie <2282756118@qq.com>
2021-09-18 11:14:31 +0800

    轮播图初始

---
+++ src/components/common/chart.taken.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="chartimg" @loade='chart()' @mousedown="down()" @mouseup="up()">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+   name: 'chart',
+   data () {
+       return {
+           
+       }
+   },
+   methods: {
+       chart() {
+
+       },
+       down() {
+
+       },
+       up() {
+           
+       }
+   }
+}
+</script>
+
+<style>
+   .chartimg{
+       width: 100%;
+       height: 200px;
+       overflow: hidden;
+   }
+   .chartimg img{
+       width: 100%;
+   }
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -5,16 +5,22 @@
            <div slot="content">聚划算</div>
            <div slot="right"></div>
         </navtag>
+        <chart>
+            <img src="~assets/img/dier.jpg" alt="">
+            <img src="~assets/img/diyi.jpg" alt="">
+        </chart>
     </div>
 </template>
 
 <script>
-import navtag from '../components/common/navtag.vue'
+import navtag from 'components/common/navtag.vue'
+import chart from 'components/common/chart.taken.vue'
 
 export default {
     name: 'home',
     components: {
         navtag,
+        chart
     }
 }
 </script>
