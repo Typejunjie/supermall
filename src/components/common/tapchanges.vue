419ede14dcffee2c32ab647ad0bea6231f680a70
Typejunjie <2282756118@qq.com>
2021-09-21 19:51:55 +0800

    小组件封装完成

---
+++ src/components/common/tapchanges.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="tapbox">
+        <span v-for="(iteam,index) in title" :key="index"
+        @click="active(index)" :class="iteamclick(index)"
+        >
+            {{iteam}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'tapchanges', 
+  props: {
+      title: Array,
+  },
+  data () {
+      return {
+          activeiteam: 0
+      }
+  },
+  methods: {
+      active(index) {
+          this.activeiteam = index
+      },
+      iteamclick(index) {
+          if(index == this.activeiteam){
+              return 'active'
+          }
+      }
+  }
+}
+</script>
+
+<style>
+.tapbox{
+    display: flex;
+    justify-content: space-around;
+    height: 40px;
+    line-height: 40px;
+}
+.tapbox span{
+    padding: 5px;
+}
+.tapbox .active{
+    border-bottom: 3px solid pink;
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -9,18 +9,21 @@
             <img src="~assets/img/dier.jpg" alt="">
             <img src="~assets/img/diyi.jpg" alt="">
         </chart>
+        <tapchanges :title="['精品','最新','潮流']"></tapchanges>
     </div>
 </template>
 
 <script>
 import navtag from 'components/common/navtag.vue'
 import chart from 'components/common/chart2.0.vue'
+import tapchanges from 'components/common/tapchanges'
 
 export default {
     name: 'home',
     components: {
         navtag,
-        chart
+        chart,
+        tapchanges,
     }
 }
 </script>
